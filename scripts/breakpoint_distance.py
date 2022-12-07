b24db1117f14bd864909b73dab5bf8de296ed05a
Henri Schmidt <packerfan73@gmail.com>
2022-12-07 09:44:40 -0500

    Bug fix.

--- scripts/breakpoint_distance.py
+++ scripts/breakpoint_distance.py
@@ -52,8 +52,9 @@ class ChromosomeCopyNumberProfile:
     profile : np.ndarray
     chromosome: str
 
-    def breakpoints(self) -> ChromosomeBreakpointProfile:
-        breakpoint_profile = np.hstack(([[1], [1]], self.profile))
+    def breakpoints(self, wgd=0) -> ChromosomeBreakpointProfile:
+        synthetic_gene = [[1 + wgd], [1 + wgd]]
+        breakpoint_profile = np.hstack((synthetic_gene, self.profile))
         breakpoint_profile = self.profile - breakpoint_profile[:, :-1] 
         return ChromosomeBreakpointProfile(
             self.bins, breakpoint_profile, self.chromosome
@@ -64,8 +65,8 @@ class CopyNumberProfile:
     """Whole-genome copy number profile"""
     profiles : list[ChromosomeCopyNumberProfile]
 
-    def breakpoints(self) -> BreakpointProfile:
-        return BreakpointProfile([p.breakpoints() for p in self.profiles])
+    def breakpoints(self, wgd=0) -> BreakpointProfile:
+        return BreakpointProfile([p.breakpoints(wgd=wgd) for p in self.profiles])
 
 """
 Computes the magnitude (i.e. distance from all 0's profile)
@@ -75,17 +76,22 @@ def breakpoint_magnitude(profile : np.ndarray) -> int:
     positive_entries = list(-1 * profile[profile > 0]) # make positive entries negative to use min-heap
     negative_entries = list(profile[profile < 0])
 
-    heapq.heapify(positive_entries)
-    heapq.heapify(negative_entries)
+    heapq.heapify(positive_entries) # invariant: all entries positive
+    heapq.heapify(negative_entries) # invariant: all entries negative
 
     distance = 0
     while len(positive_entries) > 1:
         t1 = heapq.heappop(positive_entries) # t1 < t2 (i.e -5 < -4)
         t2 = heapq.heappop(positive_entries)
-        t3 = t1 - t2
-        distance += np.abs(t2)
-        if t3 != 0:
-            heapq.heappush(positive_entries, t3)
+
+        t1 = t1 + 1
+        t2 = t2 + 1
+        distance += 1
+
+        if t1 != 0:
+            heapq.heappush(positive_entries, t1)
+        if t2 != 0:
+            heapq.heappush(positive_entries, t2)
 
     if len(positive_entries) == 1:
         distance += np.abs(positive_entries[0])
@@ -93,10 +99,16 @@ def breakpoint_magnitude(profile : np.ndarray) -> int:
     while len(negative_entries) > 1:
         t1 = heapq.heappop(negative_entries) # t1 < t2 (i.e -5 < -4)
         t2 = heapq.heappop(negative_entries)
-        t3 = t1 - t2
-        distance += np.abs(t3)
-        if t3 != 0:
-            heapq.heappush(negative_entries, t3)
+
+        t1 = t1 + 1
+        t2 = t2 + 1
+
+        distance += 1
+
+        if t1 != 0:
+            heapq.heappush(negative_entries, t1)
+        if t2 != 0:
+            heapq.heappush(negative_entries, t2)
 
     if len(negative_entries) == 1:
         distance += np.abs(negative_entries[0])
@@ -137,8 +149,8 @@ if __name__ == "__main__":
    pairwise_distances = pd.DataFrame(columns=cnp_profiles.index)
    for (n1, p1) in cnp_profiles.items():
        for (n2, p2) in cnp_profiles.items():
-           d = p1.breakpoints().distance(p2.breakpoints())
-           pairwise_distances.loc[n1, n2] = d
+           # ds = [p1.breakpoints(wgd=w1).distance(p2.breakpoints(wgd=w2)) for w1 in range(5) for w2 in range(5)]
+           pairwise_distances.loc[n1, n2] = p1.breakpoints().distance(p2.breakpoints()) # min(ds)
 
    medicc2_pairwise_distances = pd.read_csv(args.medicc2_distances, sep="\t", index_col=0)\
                                   .drop(columns=["diploid"], index=["diploid"])
@@ -160,6 +172,7 @@ if __name__ == "__main__":
    regression = stats.linregress(xs, ys)
    reg_xs = np.linspace(min(xs), max(xs), 1000)
    reg_ys = reg_xs*regression.slope + regression.intercept
+   ax.plot(reg_xs, reg_xs, linestyle="dashed", color="grey")
    ax.plot(reg_xs, reg_ys, linestyle="dashed")
    ax.text(0.6, 0.2, f"R^2 = {regression.rvalue:.4}\nP-Value = {regression.pvalue:.4}", transform=ax.transAxes)
 
