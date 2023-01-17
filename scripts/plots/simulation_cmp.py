98a822539c755892db69b7a83c40298663534219
Henri C. Schmidt <packerfan73@gmail.com>
2023-01-17 14:30:44 -0500

    Plot scripts.

--- scripts/plots/simulation_cmp.py
+++ scripts/plots/simulation_cmp.py
@@ -15,54 +15,67 @@ def parse_arguments():
 
     return parser.parse_args()
 
-if __name__ == "__main__":
-    args = parse_arguments()
-    results = pd.read_csv(args.results_csv)
-
-    fig, axes = plt.subplots(ncols=2, nrows=1, figsize=(8,4.5))
-    sns.boxplot(data=results, x='ncells', y='quartet_score', hue='algorithm', ax=axes[0])
-    sns.boxplot(data=results, x='nloci', y='quartet_score', hue='algorithm', ax=axes[1])
-
-    axes[0].legend(loc='upper left')
-    axes[0].set_ylabel("Quartet Distance")
-    axes[0].set_xlabel("# Cells")
-
-    axes[1].set_yticks([])
-    axes[1].set_ylabel(None)
-    axes[1].set_xlabel("# Loci")
-    axes[1].get_legend().remove()
-
-    fig.tight_layout()
-    fig.savefig("quartet.pdf")
+def fig2(results):
+    results = results[results['algorithm'].isin(['Breaked NJ', 'WCND', 'MEDALT', 'MEDICC2'])]
 
     fig, axes = plt.subplots(ncols=2, nrows=1, figsize=(8,4.5))
     sns.boxplot(data=results, x='ncells', y='rf_score', hue='algorithm', ax=axes[0])
-    sns.boxplot(data=results, x='nloci', y='rf_score', hue='algorithm', ax=axes[1])
 
     axes[0].legend(loc='upper left')
     axes[0].set_ylabel("RF Distance")
     axes[0].set_xlabel("# Cells")
 
-    axes[1].set_yticks([])
-    axes[1].set_ylabel(None)
-    axes[1].set_xlabel("# Loci")
-    axes[1].get_legend().remove()
+    sns.boxplot(data=results, x='ncells', y='quartet_score', hue='algorithm', ax=axes[1])
+
+    axes[1].legend(loc='upper left')
+    axes[1].set_ylabel("Quartet Distance")
+    axes[1].set_xlabel("# Cells")
 
     fig.tight_layout()
-    fig.savefig("rf.pdf")
-    plt.show()
+    fig.savefig("fig2.pdf")
+
+    print(results.groupby(['algorithm', 'ncells'])[['rf_score', 'quartet_score']].mean())
+
+def fig1(results):
+    results = results[results['algorithm'].isin(['Breaked', 'WCND', 'MEDALT', 'MEDICC2', "Sitka"])].copy()
+
+    fig, axes = plt.subplots(ncols=2, nrows=1, figsize=(8,4.5))
+    sns.boxplot(data=results, x='ncells', y='rf_score', hue='algorithm', ax=axes[0])
+
+    axes[0].legend(loc='upper left')
+    axes[0].set_ylabel("RF Distance")
+    axes[0].set_xlabel("# Cells")
+
+    sns.boxplot(data=results, x='ncells', y='quartet_score', hue='algorithm', ax=axes[1])
+
+    axes[1].legend(loc='upper left')
+    axes[1].set_ylabel("Quartet Distance")
+    axes[1].set_xlabel("# Cells")
+
+    fig.tight_layout()
+    fig.savefig("fig1.pdf")
+    print(results.groupby(['algorithm', 'ncells'])[['rf_score', 'quartet_score']].median())
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    results = pd.read_csv(args.results_csv)
+
+    rename_map = {"breaked_nni": "Breaked", 'breaked_nj': "Breaked NJ", "sitka": "Sitka", "medicc2": "MEDICC2"}
+    results['algorithm'] = results.algorithm.map(lambda x: rename_map[x] if x in rename_map else x)
+
+    fig1(results)
 
     fig, ax = plt.subplots(figsize=(4, 4.5))
-    timing_results_df = results[results['algorithm'].isin(['breaked_nj', 'breaked_nni', 'medicc2', 'WCND', 'MEDALT'])].copy()
+    timing_results_df = results[results['algorithm'].isin(['Breaked', 'MEDICC2', 'WCND', 'MEDALT', "Sitka"])].copy()
     timing_results_df = timing_results_df[
-        (timing_results_df['nloci'] == 4000) & (timing_results_df['ncells'] <= 150)
+        (timing_results_df['nloci'] == 4000)
     ]
     timing_results_df['log(s)'] = np.log10(timing_results_df['s'])
-    sns.boxplot(data=timing_results_df, x='ncells', y='log(s)', hue='algorithm', ax=ax )
+    sns.swarmplot(data=timing_results_df, x='ncells', y='log(s)', hue='algorithm', ax=ax )
     ax.set_ylabel("log10(time) (s)")
     ax.set_xlabel("# Cells")
-    ax.legend(loc='upper left')
+    ax.legend(loc='upper right')
 
     fig.tight_layout()
-    fig.savefig("timing.pdf")
+    fig.savefig("fig1_timing.pdf")
     plt.show()
